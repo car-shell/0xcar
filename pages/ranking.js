c0f9d2fe42bcf1cfdb9241dcad5bd9ed2db7b9c0
eazlong <eazlong@gmail.com>
2023-08-20 14:37:34 +0800

    remove mainpage

--- pages/ranking.js
+++ pages/ranking.js
@@ -1,12 +1,15 @@
 import Ranking from '../components/MainPage/Ranking'
 import Layout from '../components/Layout/Layout'
 import styles from '../styles/MainPage.module.css'
+import t from '../styles/Bet.module.css'
 
 export default function Home() {
   return (
-    <Layout showMenu={true}>
-        <div className={`${styles.container} ${styles.flex_column}`}>
-            <Ranking width='70%'/>
+    <Layout showMenu={true} showFooter={false}>
+        <div className={t.container} style={{height: 'calc(100vh - 72px'}}>
+          <div className={`${styles.container} ${styles.flex_column}`} >
+              <Ranking width='70%'/>
+          </div>
         </div>
     </Layout>
   )
