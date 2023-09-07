1ec7927152cd4382342441452301b5664c8fd9fa
eazlong <eazlong@gmail.com>
2023-09-07 21:42:40 +0800

    points ranking

--- components/MainPage/Ranking.js
+++ components/MainPage/Ranking.js
@@ -32,7 +32,7 @@ export default function Ranking({width = '920px'}) {
   const [betRows, setBetRows] = React.useState([]);
   const [pointsRows, setPointsRows] = React.useState([]);
   const [showPointsRules, setShowPointsRules] = React.useState(false);
-  const pointsRulesRef = React.useRef()
+  // const pointsRulesRef = React.useRef()
 
   const {chain, chains} = useNetwork()
   const chainId = React.useMemo(()=>{ return chain != undefined && chain?.id &&  chains.map(c=>c.id).indexOf(chain.id) != -1 ? chain.id : defaultChainId}, [chain])
@@ -41,23 +41,6 @@ export default function Ranking({width = '920px'}) {
   const handleChange = (event, newValue) => {
       setValue(newValue);
   };
-
-  const getPointsLogs = React.useCallback(async ()=>{
-    try {
-      const logs = await getUrl("/points_ranking")
-      console.log(logs);
-      const r = logs.data.map((item, i)=>{
-        return {ranking: item['last_24_ranking'], address: item['player'], bet_count: item['last_24_bet_counts'],
-            boost: item['last_24_ranking']<10?2:item['last_24_ranking']<20?1.5:1.2,
-            bet_amount: item["last_24_bet_amounts"], points: item['last_24'], total_points: item['total']}
-      })
-      
-      setPointsRows(r)
-    } catch (error) {
-      console.log(error);
-    }
-      
-  }, [winRows])
 
   const getWinLogs = React.useCallback(async ()=>{
     try {
@@ -79,7 +62,8 @@ export default function Ranking({width = '920px'}) {
       const logs = await getUrl("/bet_ranking", {params : {type: "bet"}})
       console.log(logs);
       const r = logs.data.map((item, i)=>{
-        return createData(i+1, item["address"], item["bet_count"], item["total_bet"])
+        return { id: i+1, ranking: i+1, address: item["address"], count: item["bet_count"], total: item["total_bet"],
+            total_points: item['total_point'] }; //createData(i+1, item["address"], item["bet_count"], item["total_bet"])
       })
       setBetRows(r)
     } catch (error) {
@@ -89,11 +73,11 @@ export default function Ranking({width = '920px'}) {
   }, [betRows])
 
   React.useEffect(()=>{
-    value==0?getWinLogs():getBetLogs()
-    getPointsLogs()
+    value==0?getBetLogs():getWinLogs()
+    // getPointsLogs()
     const i = setInterval(() => {
       value==0?getWinLogs():getBetLogs()
-      getPointsLogs()
+      // getPointsLogs()
     }, 30000);
 
     return ()=>clearInterval(i)
@@ -115,17 +99,22 @@ export default function Ranking({width = '920px'}) {
                 }
             },
             {
-                Header: "Win Count",
+                Header: "Bet Count",
                 accessor: "count",
                 align: "center",
             },
             {
-                Header: "Total Netwin",
+                Header: "Bet Amount",
                 accessor: "total",
                 align: "center",
                 format: (x)=>{
                   return formatAmount(x)
                 }
+            },
+            {
+                Header: "Total Points",
+                accessor: "total_points",
+                align: "center"
             }
         ],
         []
@@ -262,21 +251,24 @@ export default function Ranking({width = '920px'}) {
       <Stack direction='row' alignItems='baseline' sx={{columnGap: '4px'}} >
         <Image src='./fire.png' width='32' height='32' />
         <Typography component='div' sx={{marginTop: '18px', font: '700 normal 36px Arial'}}>
-          ROLLING <span style={{color: "red"}}>24H</span> LEADERBOARD
+          ROLLING LEADERBOARD: Phase 1
         </Typography>
       </Stack>
       <Typography component='div' sx={{marginTop: '18px', font: '700 normal 18px Arial'}} color='#aaaaaa'>
-        Climb the leaderboard to amplify your points! The top 50 within a 24-hour window earn a bonus!
+        Climb the leaderboard to boost your points!
+      </Typography>
+      <Typography component='div' sx={{ font: '700 normal 18px Arial'}} color='#aaaaaa'>
+        The top 50 during the event will receive exclusive NFTs and special role rewards
       </Typography>
       <Box sx={{ borderBottom: 1, borderColor: 'divider', margin: '68px 0 0 0px' }} >
           <StyledTabs onChange={handleChange} value={value} selectionFollowsFocus={true}>
+            {/* <StyledTab disableRipple label="Points Ranking" index={0} /> */}
             <StyledTab disableRipple label="Points Ranking" index={0} />
             <StyledTab disableRipple label="Winning Ranking" index={1} />
-            <StyledTab disableRipple label="Bet Ranking" index={2} />
           </StyledTabs>
       </Box>
       
-      <Box hidden={value!==0} sx={{
+      {/* <Box hidden={value!==0} sx={{
         width: width,
       }}>
         <Typography ref={pointsRulesRef} component='div' sx={{font: '400 normal 14px Arial', textAlign: 'right', cursor:'pointer'}} color='#8080FF' onClick={()=>setShowPointsRules(!showPointsRules)}>
@@ -290,7 +282,7 @@ export default function Ranking({width = '920px'}) {
         }}>
         <StickyHeadTable columns={pointsColumns} data={pointsRows} maxHeight={null}/>
         </Box>
-      </Box>
+      </Box> */}
       <Box hidden={value!==1} sx={{
         width: width,
         border: '1px solid #333333',
@@ -300,7 +292,7 @@ export default function Ranking({width = '920px'}) {
       }}>
         <StickyHeadTable columns={winColumns} data={winRows} maxHeight={null}/>
       </Box>
-      <Box hidden={value!==2} sx={{
+      <Box hidden={value!==0} sx={{
         width: width,
         border: '1px solid #333333',
         borderRadius: '5px',
@@ -309,7 +301,7 @@ export default function Ranking({width = '920px'}) {
       }}>
         <StickyHeadTable columns={betColumns} data={betRows} maxHeight={null}/>
       </Box>
-      {showPointsRules && <Box sx={{ position: 'absolute',
+      {/* {showPointsRules && <Box sx={{ position: 'absolute',
           right: pointsRulesRef.current.getBoundingClientRect().left,
           top: pointsRulesRef.current.getBoundingClientRect().bottom,
           width: "360px",
@@ -332,7 +324,7 @@ export default function Ranking({width = '920px'}) {
           <Typography component='div' sx={{ font: '400 normal 14px sans'}}>
             * Users ranked 31-50 will get 1.2x their POINTS earned in 24H
           </Typography>
-      </Box>}
+      </Box>} */}
     </React.Fragment>
   );
 }
