06ce463a9423ed44109112aa10cb9b9e33298b83
eazlong <eazlong@gmail.com>
2023-09-11 11:44:28 +0800

    new contract

--- components/Bet/Pool.js
+++ components/Bet/Pool.js
@@ -45,11 +45,11 @@ const Pool = () => {
       </div> */}
       <div className={styles.info}>
        <div className={styles.info_item}>
-        <div className={styles.title}> Initial Pool Balance</div>
+        <div className={styles.title}> Initial Pool Fund</div>
         <div className={styles.content}>{poolDetails?formatAmount(poolDetails[0].div(ethers.BigNumber.from('1000000000000000000'))):'--'} <span style={{color: '#7F7F7F', fontSize: "12px"}}> {token?.symbol} </span></div>
        </div>
        <div className={styles.info_item}>
-         <div className={styles.title}> Remaining Pool Balance</div>
+         <div className={styles.title}> Current Pool Balance</div>
          <div className={styles.content}>{poolDetails?formatAmount(poolDetails[1].div(ethers.BigNumber.from('1000000000000000000'))):'--'} <span style={{color: '#7F7F7F', fontSize: "12px"}}> {token?.symbol} </span></div>
        </div>
        <div className={styles.info_item}>
